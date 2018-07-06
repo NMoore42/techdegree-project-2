e3ee436303aab688a9a556020881debcf7e907ac
Nick Moore <nicholas.moore5@icloud.com>
2018-07-06 18:39:39 -0400

    Added appendPage function

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
 
 const $studentItem = $('.student-item');
-$studentItem.hide();
 
-function showPage (){
-  for (i = 0; i < $studentItem.length; i += 1) {
-    if (i<10){
-      $studentItem[i].show();
+
+//Displays 10 students per page
+function showPage(pageNumber){
+  $studentItem.hide();
+  for (i = 0; i <= $studentItem.length; i += 1) {
+    if (i >= pageNumber * 10 && i <= (pageNumber * 10) + 9){
+      $studentItem.eq(i).show();
     }
   }
 }
 
-showPage();
+showPage(0);
+
+function appendPageLinks(){
+  let pageNumbers = Math.ceil($studentItem.length/10);
+  let $pageLink = $('.pagination ul');
+  let li = document.createElement('li');
+  for (i=0; i<= pageNumbers; i += 1){
+    $pageLink.appendChild(li[i]);
+  }
+}
+
+appendPageLinks();
