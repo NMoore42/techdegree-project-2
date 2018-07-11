4d69fddde0faa71a7220b6bd820f778166fcf384
Nick Moore <nicholas.moore5@icloud.com>
2018-07-11 15:19:55 -0400

    Added event listener click
    Function at meets expectations

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const $studentItem = $('.student-item');
 const pageNumbers = Math.ceil($studentItem.length/10);
 const pageLink = document.getElementsByClassName('pagination')[0];
 const ul = document.createElement('ul');
-const button = document.querySelector('div.pagination').children;
+
+
 
 
 //Displays 10 students per page
@@ -20,7 +21,6 @@ function showPage(pageNumber){
 //Appends page link buttons to DOM dynamically
 function appendPageLinks(){
   pageLink.appendChild(ul);
-  console.log(pageNumbers);
   for (let i=0; i< pageNumbers; i += 1){
     let a = document.createElement('a');
     let li = document.createElement('li');
@@ -28,15 +28,16 @@ function appendPageLinks(){
     a.setAttribute('href', '#');
     ul.appendChild(li);
     li.appendChild(a);
+    //Event listener dislays section of students corresponding to buttons
+    a.addEventListener('click', function() {
+      let clickedPage = a.textContent-1;
+      a.setAttribute('class', 'active');
+      showPage(clickedPage);
+    });
   }
 }
 
-//Event listener dislays section of students corresponding to buttons
-button.addEventListener('click', showPage(clickedPage) {
-  let a = document.querySelectorAll("A");
-  clickedPage = a.textContent;
-  a.setAttribute('class', 'active');
-});
+
 
 showPage(0);
 appendPageLinks();
